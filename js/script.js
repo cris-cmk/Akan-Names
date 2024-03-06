3942de7936f8f08141f84dcfdfde8bf09e47582f
cris-cmk <muthuikibara@gmail.com>
2024-03-06 16:07:16 +0300

    validate input fields

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('akanForm');
     form.addEventListener('submit', function(event) {
-
+        event.preventDefault();
         const birthdate = new Date(document.getElementById('birthdate').value);
         const gender = document.getElementById('gender').value;
+       if(gender == "select")
+       {
+        alert("Please Select Gender ");
+       }
 
-        const dayOfWeek = birthdate.getDay(); // 0 (Sunday) to 6 (Saturday)
-        let akanName;
-
-        // Determine Akan name based on gender and day of the week
-        if (gender === 'male') {
-            akanName = getAkanMaleName(dayOfWeek);
-        } else {
-            akanName = getAkanFemaleName(dayOfWeek);
-        }
-
-        // Display the result
-        displayResult(akanName);
     });
 });
